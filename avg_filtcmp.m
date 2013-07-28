f4aa8b383cd39140f7596fc1794670603b668651
Christopher L Wynkoop <chr1s.wynk00p@gmail.com>
2013-07-27 22:49:30 -0500

    adding comments to octave scripts

--- avg_filtcmp.m
+++ avg_filtcmp.m
@@ -1,19 +1,27 @@
-Ts = 0.01;
-fc = 1;
-tauF = 1/(2*pi*fc);
-alpha = Ts/(Ts+tauF)
-B = [0 alpha];
-A = [1 -(1-alpha)];
-[H, W] = freqz(B,A);
-[Havg,Wavg] = freqz( ones(1,10)/10, 1 );
-F = ((W+1e-6)/pi)*(1/(2*Ts));
-Favg = ((Wavg+1e-6)/pi)*(1/(2*Ts));
-h = figure;
-plot( Favg, 20*log10(abs(Havg)),'LineWidth',2, F, 20*log10(abs(H)),'LineWidth',2 )
+% Script to compare an moving average filter
+% with a single pole low pass filter
+Ts = 0.01;  % sample at 100 Hz
+fc = 1;     % set the cutoff at 1 Hz
+tauF = 1/(2*pi*fc);   % angular period for the cutoff
+alpha = Ts/(Ts+tauF); % time domain filter coefficient
+B = [0 alpha];        % numerator for digital frequency transfer function
+A = [1 -(1-alpha)];   % denominator for digital frequency transfer function
+[H, W] = freqz(B,A);  % calculate the frequency response for the single
+                      % pole filter
+[Havg,Wavg] = freqz( ones(1,10)/10, 1 ); % calculate the frequency response
+                                         % for the averaging filter
+F = ((W+1e-6)/pi)*(1/(2*Ts));            % get the frequency in Hz for
+Favg = ((Wavg+1e-6)/pi)*(1/(2*Ts));      % both filters
+h = figure;                              % get a new figure and save the handle
+% plot the frequency response of both filters
+plot( Favg, 20*log10(abs(Havg)),'LineWidth',2,...
+      F, 20*log10(abs(H)),'LineWidth',2 )
+% add notations to the plot
 legend('Averaging Filter','Single Pole Filter')
 title('Averaging/Single Pole Filter Frequency Response')
 xlabel('Frequency (Hz)')
 ylabel('Magnitude (dB)')
+% write the plot to a PNG file
 print(h,'-dpng','-color','filtcmp.png')
 
 h = figure;
--- dfilt.m
+++ dfilt.m
@@ -1,12 +1,15 @@
-Ts = 0.5;
-fc = 0.1;
-tauF = 1/(2*pi*fc);
-alpha = Ts/(Ts+tauF)
-B = [0 alpha];
-A = [1 -(1-alpha)];
-[H, W] = freqz(B,A);
-h = figure;
+% Script plot the frequency response of a single pole low pass filter
+Ts = 0.5;            % sample at 2 Hz
+fc = 0.1;            % set the cutoff at 0.1 Hz
+tauF = 1/(2*pi*fc);  % angular period for the cutoff
+alpha = Ts/(Ts+tauF);% time domain filter coefficient
+B = [0 alpha];       % numerator for digital frequency transfer function
+A = [1 -(1-alpha)];  % denominator for digital frequency transfer function
+[H, W] = freqz(B,A); % calculate the frequency response for the single
+h = figure;          % get a new figure and save the handle
+% plot the frequency response
 semilogx( ((W+1e-6)/pi)*(1/(2*Ts)), 20*log10(abs(H)),'LineWidth',4)
+% add notations to the plot
 title('Digital Filter Frequency Response')
 ylabel('Magnitude (dB)')
 xlabel('Frequency (Hz)')
