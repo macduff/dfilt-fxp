fe9b1884ea107ee9add6ab3e1eefea1d8b293b65
Christopher L Wynkoop <chr1s.wynk00p@gmail.com>
2013-07-27 22:28:56 -0500

    commenting filter routine

--- dfilt_fxp/dfilt_fxp.c
+++ dfilt_fxp/dfilt_fxp.c
@@ -1,14 +1,25 @@
 #include <stdint.h>
 #include "dfilt_fxp.h"
 
+/* dfilt_single_pole
+ *  Parameters:
+ *    input - current input to the filter
+ *    output - previous time step output of the filter
+ *    alpha - cutoff frequency parameter, scaled by left shifted 15
+ *  returns:
+ *    current time step output of the filter
+ *  Calculates the current output of the digital filter.
+ */
 uint16_t
 dfilt_single_pole(uint16_t input, uint16_t prev_output, uint16_t alpha)
 {
-  uint16_t output;
-  uint32_t a1 = ((1<<15)-alpha);
-  uint32_t b0 = alpha;
-  uint32_t y1 = (a1*prev_output) >> 15;
-  uint32_t u1 = (b0*input) >> 15;
+  uint16_t output;                  /* current time step output */
+  uint32_t beta = ((1<<15)-alpha);  /* coefficient of previous output */
+  /* calculate the previous output term and scale to output scaling */
+  uint32_t y1 = (beta*prev_output) >> 15;
+  /* calculate the input term and scale to output scaling */
+  uint32_t u1 = (alpha*input) >> 15;
+	/* calculate current filter output */
   output = y1 + u1;
 	return output;
 }
--- dfilt_fxp/dfilt_fxp.h
+++ dfilt_fxp/dfilt_fxp.h
@@ -2,7 +2,14 @@
 #define DFILT_FXP_H_
 
 #include <stdint.h>
-
+/* dfilt_single_pole
+ *  Parameters:
+ *    input - current input to the filter
+ *    output - previous time step output of the filter
+ *    alpha - cutoff frequency parameter, scaled by left shifted 15
+ *  returns:
+ *    current time step output of the filter
+ */
 uint16_t
 dfilt_single_pole(uint16_t input, uint16_t prev_output, uint16_t alpha);
 
